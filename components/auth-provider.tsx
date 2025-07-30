aeaffe445b3da202dddbb0e72627a5a8e5fb2f54
DESKTOP-D9IOURO\31733 <huangcherie@163.com>
2025-07-30 17:47:35 +0800

    supabase初步配置

---
+++ components/auth-provider.tsx
@@ -0,0 +1,8 @@
+'use client'
+
+import { createContext, useContext} from 'react'
+
+
+export function AuthProvider({ children }: any) {
+  return <AuthProvider>{children}</AuthProvider>
+}
---
+++ lib/supabase/client.ts
@@ -0,0 +1,24 @@
+/*
+* @Author: Cherie
+* @Date: 2025-07-30 16:20:26
+* @Description:'supabase使用 js 实例'
+*/
+// import { createClient } from '@supabase/supabase-js'
+// Create a single supabase client for interacting with your database
+// const supabase = createClient('https://xyzcompany.supabase.co', 'public-anon-key')
+
+
+import {createClient} from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+
+// Create a single supabase client for interacting with your database
+let supabaseClient: ReturnType<typeof createClient> | null = null
+
+export const getSuabaseClient = () => {
+  if (!supabaseClient) {
+    supabaseClient = createClient(supabaseUrl, supabaseAnonKey)
+  }
+  return supabaseClient
+}
