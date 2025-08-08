ddd5ffe9d0a7125a3bdb409226c56023c400aa23
nicecherie <huangcherie@163.com>
2025-08-08 21:45:26 +0800

    添加全局loading，后面看怎么用

---
+++ components/loading.tsx
@@ -0,0 +1,27 @@
+// 创建一个 loading provider
+import { createContext, useContext, useState } from 'react'
+interface LoadingProvider {
+  isLoading: boolean
+  setIsLoading: (isLoading: boolean) => void
+}
+
+export const LoadingContext = createContext<LoadingProvider | undefined>(
+  undefined
+)
+
+export function LoadingProvider({ children }: { children: React.ReactNode }) {
+  const [isLoading, setIsLoading] = useState(false)
+
+  return (
+    <LoadingContext.Provider value={{ isLoading, setIsLoading }}>
+      {children}
+    </LoadingContext.Provider>
+  )
+}
+export function useLoading() {
+  const context = useContext(LoadingContext)
+  if (context === undefined) {
+    throw new Error('useLoading must be used within a LoadingProvider')
+  }
+  return context
+}
